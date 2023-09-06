a70e8765852e1ac6126bbd29c1b88c455800ecd2
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-09-06 12:34:52 -0300

    First Commit

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Container, HeroStyle, Titulo } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <HeroStyle>
+    <Container>
+      <Titulo>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </Titulo>
+    </Container>
+  </HeroStyle>
 )
 
 export default Hero
